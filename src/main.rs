0a478be5abf0d80fb4cda85eeb16dada0e314b35
Suprakon <taresupakon@gmail.com>
2023-10-15 01:40:15 +0700

    solved easy 27.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod easy14;
 mod easy20;
  mod array {
    pub mod easy26;
+   pub mod easy27;
 }
 
 fn main() {
@@ -9,5 +10,6 @@ fn main() {
 
    // print!("{}", easy20::Solution::is_valid("[[]][".to_string()));
 
-   print!("{}", array::easy26::Solution::remove_duplicates(&mut [1,1,1,2,3,4,4,5,6].to_vec()));
+   // print!("{}", array::easy26::Solution::remove_duplicates(&mut [1,1,1,2,3,4,4,5,6].to_vec()));
+   print!("{}", array::easy27::Solution::remove_element(&mut [0,1,2,2,3,0,4,2].to_vec(), 2));
 }
