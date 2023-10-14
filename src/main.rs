332b4ec48d21e59621246573764ccea764809ba9
Suprakon <taresupakon@gmail.com>
2023-10-14 17:33:10 +0700

    solved easy26

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 mod easy14;
 mod easy20;
+ mod array {
+   pub mod easy26;
+}
 
 fn main() {
    // print!("{}", easy14::Solution::longest_common_prefix(["flow".to_owned(), "flower".to_owned(), "flo".to_owned()].to_vec()));
 
-   print!("{}", easy20::Solution::is_valid("[[]][".to_string()));
+   // print!("{}", easy20::Solution::is_valid("[[]][".to_string()));
+
+   print!("{}", array::easy26::Solution::remove_duplicates(&mut [1,1,1,2,3,4,4,5,6].to_vec()));
 }
