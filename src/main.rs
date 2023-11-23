24fc29d3e87077b4415cda1ebcdd08b803097d2d
Suprakon <taresupakon@gmail.com>
2023-11-23 16:51:11 +0700

    solved two pointer problem, remove duplicates.

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,16 @@ mod hash_table {
    pub mod easy169;
 }
 
+mod algo_monster {
+   pub mod two_pointers {
+      pub mod remove_duplicates;
+   }
+}
+
 fn main() {
 
    array::easy35::Solution::run_units_test_easy_35();
    array::meduim11::Solution::run_test_cases();
    hash_table::easy169::Solution::run_test_cases();
+   algo_monster::two_pointers::remove_duplicates::Solution::run_test_cases();
 }
